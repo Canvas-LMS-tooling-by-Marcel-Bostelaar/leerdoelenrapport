d03ff5d18ebc98919c36be1b8491e022a67986ad
Marcel Bostelaar <marcel.bostelaar@gmail.com>
2026-01-10 13:40:29 +0100

    Basic setup

---
+++ src/middleware.php
@@ -0,0 +1,3 @@
+<?php
+
+// $router->aliasMiddleware('auth', AuthMiddleware::class);
---
+++ src/public/index.php
@@ -0,0 +1,25 @@
+<?php
+
+require __DIR__ . '/../../vendor/autoload.php';
+
+use Illuminate\Container\Container;
+use Illuminate\Events\Dispatcher;
+use Illuminate\Routing\Router;
+use Illuminate\Http\Request;
+
+$container = new Container;
+$events = new Dispatcher($container);
+
+$container->bind(
+    Illuminate\Routing\Contracts\CallableDispatcher::class,
+    Illuminate\Routing\CallableDispatcher::class
+);
+
+$router = new Router($events, $container);
+
+require __DIR__ . '/../middleware.php';
+require __DIR__ . '/../routes.php';
+
+$request = Request::capture();
+$response = $router->dispatch($request);
+$response->send();

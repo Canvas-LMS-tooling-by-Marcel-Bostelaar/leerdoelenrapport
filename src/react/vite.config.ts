687f2d537983560637a40d27025313dfb2aa2f15
Marcel Bostelaar <marcel.bostelaar@gmail.com>
2026-01-10 17:19:23 +0100

    Fixed vite build

--- src/react/vite.config.ts
+++ src/react/vite.config.ts
@@ -11,7 +11,14 @@ export default defineConfig({
     }),
   ],
   build: {
-    outDir: '../public/static/react',
+    outDir: '../backend/public/static/react',
     emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        entryFileNames: '[name].js',
+        chunkFileNames: '[name].js',
+        assetFileNames: '[name].[ext]',
+      },
+    },
   },
 })

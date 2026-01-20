a011479af16d10f9ace75892c6cf7a1278561d25
Marcel Bostelaar <marcel.bostelaar@gmail.com>
2026-01-20 17:33:46 +0100

    endpoint for config

--- src/backend/helpers.php
+++ src/backend/helpers.php
@@ -1,7 +1,9 @@
 <?php
 
 use App\Middleware\Util\ProviderContainer;
+use CanvasApiLibrary\Core\Models\CourseStub;
 use Illuminate\Container\Container;
+use Illuminate\Routing\ResponseFactory;
 
 if (!function_exists('providers')) {
     /**
@@ -15,5 +17,41 @@ if (!function_exists('providers')) {
             throw new \RuntimeException('Providers container is not bound. Ensure the NonCachingProviderSetup middleware runs before accessing providers().');
         }
         return $app->make('api.providers');
+    }
+}
+
+if (!function_exists('course')) {
+    /**
+     * Retrieve the typed providers container.
+     */
+    function course(): CourseStub
+    {
+        /** @var Container $app */
+        $app = Container::getInstance();
+        if (! $app->bound('api.course')) {
+            throw new \RuntimeException('Course container is not bound. Ensure the NonCachingProviderSetup middleware runs before accessing course().');
+        }
+        return $app->make('api.course');
+    }
+}
+
+
+if (! function_exists('response')) {
+    /**
+     * Return a new response from the application.
+     *
+     * @param  \Illuminate\Contracts\View\View|string|array|null  $content
+     * @param  int  $status
+     * @return ($content is null ? \Illuminate\Contracts\Routing\ResponseFactory : \Illuminate\Http\Response)
+     */
+    function response($content = null, $status = 200, array $headers = []): ResponseFactory|IlluminateResponse
+    {
+        $factory = app(ResponseFactory::class);
+
+        if (func_num_args() === 0) {
+            return $factory;
+        }
+        /** @var ResponseFactory $factory */
+        return $factory->make($content ?? '', $status, $headers);
     }
 }
--- src/backend/routes.php
+++ src/backend/routes.php
@@ -1,8 +1,13 @@
 <?php
 
+use CanvasApiLibrary\Core\Models\Course;
+use CanvasApiLibrary\Core\Models\Domain;
+use Illuminate\Container\Container;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use Illuminate\Routing\Router;
 use Illuminate\Support\Facades\Redirect;
+use illuminate\Support;
 
 function routes(Router $router)
 {
@@ -16,6 +21,28 @@ function routes(Router $router)
         return "hello world";
     });
 
+    $router->group(['prefix' => 'api'], function (Router $router) {
+        $router->get('config', function(){
+            $course = course();
+            $providers = providers();
+            return json_encode($providers->configProvider->getConfigInCourse($course));
+        });
+
+        $router->post('config', function(Request $request){
+            $course = course();
+            $providers = providers();
+            $configData = $request->get("config");
+            $decoded = json_decode($configData, true);
+            if($decoded === null){
+                return new Response('Invalid JSON', 400);
+            }
+            $providers->configProvider->saveConfig($course, $configData);
+            return json_encode(['status' => 'success']);
+        });
+    });
+
+
+    //dev utilities
     $router->get('cache', function(){
         if(session_status() == PHP_SESSION_NONE){
             session_start();
@@ -34,6 +61,24 @@ function routes(Router $router)
         }
         unset($_SESSION["urls"]);
         return "Cache cleared. <a href='/cache'>Go back</a>";
+    });
+
+    //utility function to set the current course in session, for when app is not yet integrated into Canvas
+    $router->post('setCourse', function(Request $request){
+        $domain = base64_decode($request->input('domain'));
+        $courseId = $request->input('courseId');
+        $domain = new Domain($domain);
+        $course = new Course();
+        $course->id = $courseId;
+        $course->domain = $domain;
+
+        if(session_status() == PHP_SESSION_NONE){
+            session_start();
+        }
+        $app = Container::getInstance();
+        $app->instance('api.course', $course);
+    
+        return "Course set to " . $courseId . " on domain " . $domain->domain;
     });
 
 }
